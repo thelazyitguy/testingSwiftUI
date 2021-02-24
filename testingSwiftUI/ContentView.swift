c5b6fcb199cc9295bb81faaca0f3ef2e3edc01b9
TheLazyITGuy <60739640+thelazyitguy@users.noreply.github.com>
2021-02-24 11:24:58 -0500

    Initial Commit

---
+++ testingSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testingSwiftUI
+//
+//  Created by Austin Pape on 2/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testingSwiftUI/testingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingSwiftUIApp.swift
+//  testingSwiftUI
+//
+//  Created by Austin Pape on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct testingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
